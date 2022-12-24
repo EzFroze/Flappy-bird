0e54151138c311eb425e64e0cfd4ef3711bac67e
Rustam Sultanbekov <sultanbekov-01@yandex.ru>
2022-12-25 01:13:57 +0300

    [BAP-41]: Добавил страницы

---
+++ packages/client/src/pages/game/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Game: React.FC = () => {
+  return <div>Тут игра (возможно стоит поменять название папки и компонента)</div>
+}
---
+++ packages/client/src/pages/leaderboard/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Leaderboard: React.FC = () => {
+  return <div>Список лучших (лидерборд)</div>
+}
