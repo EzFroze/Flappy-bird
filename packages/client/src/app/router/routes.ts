e6a2dc839591ea1b833e938fb63ebbac4e1fb6f2
Rustam Sultanbekov <sultanbekov-01@yandex.ru>
2023-01-18 00:40:19 +0300

    [BAP-68]: удалил дублирующийся файл

--- packages/client/src/app/router/routes.ts
+++
@@ -1,9 +0,0 @@
-export enum RoutesEnum {
-  SignIn = '/',
-  SignUp = '/sign-up',
-  Profile = '/profile',
-  Password = '/password',
-  Forums = '/forums',
-  ServerError = '/server-error',
-  NotFound = '/not-found',
-}
