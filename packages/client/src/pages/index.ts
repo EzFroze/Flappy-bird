daf8f4d4db30e1fe2f7d96738ca21e8e4fcd777b
codenotype <codenotype@gmail.com>
2023-02-22 21:56:55 +0300

    #19: добавлен роутинг без ошибок (без стора)

---
+++ packages/client/src/pages/index.ts
@@ -0,0 +1,29 @@
+import { ExamplePage } from "./ExamplePage";
+import { ForumCreateThreadPage } from "./ForumCreateThreadPage";
+import { ForumsPage } from "./ForumsPage";
+import { ForumThreadPage } from "./ForumThreadPage";
+import { GamePage } from "./GamePage";
+import { LeaderboardPage } from "./LeaderboardPage";
+import { NotFoundPage } from "./NotFoundPage";
+import { PasswordPage } from "./PasswordPage";
+import { ProfileChangePage } from "./ProfileChangePage";
+import { ProfilePage } from "./ProfilePage";
+import { ServerErrorPage } from "./ServerErrorPage";
+import { SignInPage } from "./SignInPage";
+import { SignUpPage } from "./SignUpPage";
+
+export {
+  SignInPage,
+  SignUpPage,
+  ExamplePage,
+  NotFoundPage,
+  ServerErrorPage,
+  ProfilePage,
+  ProfileChangePage,
+  PasswordPage,
+  ForumsPage,
+  ForumThreadPage,
+  ForumCreateThreadPage,
+  LeaderboardPage,
+  GamePage
+}
