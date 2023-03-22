e0cab7fa0b37f6ad25674120518da69a6e2319f2
SilantevR <craft_89@inbox.ru>
2023-03-22 09:33:29 +0300

    ssl

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -35,6 +35,7 @@ echo
 
 echo "### Starting nginx ..."
 docker-compose up --force-recreate -d nginx
+docker-compose up --force-recreate -d client
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
